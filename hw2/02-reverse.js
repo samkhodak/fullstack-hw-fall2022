65ee653d6c4a53b01a58a9dc1d5b56f3faf9e544
samkhodak <samkhodak@gmail.com>
2022-10-08 18:49:00 -0700

    completed hw2-02

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,27 @@
 /** Exercise 02 - Reverse **/
 
-// Add your code here
+let number_input = document.getElementById("input");
+let submit_button = document.getElementById("reverse");
+let out = document.createElement('div');
+submit_button.parentElement.append(out);
+
+submit_button.addEventListener("click", () => {   //If there is no form, the "submit" action is not triggered, so "submit" as the action in event listener would not work.
+
+    out.style.margin = "1.5em 0 0.5em 0";
+
+    //Reverse the number and output it
+    let number = number_input.value.split("");            
+    if (number.length === 8)
+    {
+      reversed = number.reverse();
+      reversed = number.join("");
+      out.textContent = `${number.join("")} --> ${reversed}`;
+      out.style.color = "green";
+
+    }
+    else
+    {
+      out.textContent = `Error: Please enter an 8-digit number`;
+      out.style.color = "red";
+    }
+})
